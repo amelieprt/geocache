a184448cf0718e70f63cb30d7f996803a10e27dc
amelieprt <105794283+amelieprt@users.noreply.github.com>
2025-02-20 15:07:40 +0100

    1

--- ts/serverDB.ts
+++ ts/serverDB.ts
@@ -1,9 +1,9 @@
 import { MongoClient } from 'mongodb';
 
 // parametres du serveur de données
-// const uri = "mongodb://localhost:27017";
+const uri = "mongodb://localhost:27017";
 // const uri = process.env.SCALINGO_MONGO_URL || "mongodb://127.0.0.1:27017/ma_nouvelle_bdd";
-const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/ma_nouvelle_bdd';
+// const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/ma_nouvelle_bdd';
 
 
 const dbname = "ma_nouvelle_bdd";
@@ -11,7 +11,7 @@ const dbname = "ma_nouvelle_bdd";
 // connexion à la base de données
 let db: any
 {
-    let client = new MongoClient(mongoURI);
+    let client = new MongoClient(uri);
     try {
         // Connect to the MongoDB cluster
         client.connect();
